25f6eb504eca71b59fba9b1bf80e5a62f7faf884
michellejzh <michelle_zheng@brown.edu>
2015-12-01 18:29:23 -0500

    added filtering

---
+++ filtering/apply_filters.m
@@ -0,0 +1,12 @@
+function filtered_video = apply_filters(video, filters)
+vidDim = size(video);
+x = vidDim(1);
+y = vidDim(2);
+t = vidDim(4);
+filtered_video = zeros(x,y,3,t,length(filters));
+display(size(filtered_video));
+for d=1:length(filters),
+    curr = imfilter(video,filters{d,1});
+	filtered_video(:,:,:,:,d) = curr;
+end
+end
