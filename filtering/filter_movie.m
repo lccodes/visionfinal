1b3f3923fe63bdafcda0b73f7b59093673f13e4c
michellejzh <michelle_zheng@brown.edu>
2015-12-01 21:30:37 -0500

    saves

--- filtering/filter_movie.m
+++ filtering/filter_movie.m
@@ -18,29 +18,43 @@ gabors = reshape(gabors, [8,1]);
 
 %% Load and preprocess the video
 path1 = 'handclapping.mp4';
-[video4, frameNum] = load_and_preprocess_video(path1);
+[video1, frameNum] = load_and_preprocess_video(path1);
 
-path2 = 'handclapping.mp4';
-[video4, frameNum] = load_and_preprocess_video(path2);
-
-path3 = 'handclapping.mp4';
-[video4, frameNum] = load_and_preprocess_video(path3);
-
-path4 = 'handclapping.mp4';
-[video4, frameNum] = load_and_preprocess_video(path4);
+% path2 = 'handclapping.mp4';
+% [video2, frameNum] = load_and_preprocess_video(path2);
+% 
+% path3 = 'handclapping.mp4';
+% [video3, frameNum] = load_and_preprocess_video(path3);
+% 
+% path4 = 'handclapping.mp4';
+% [video4, frameNum] = load_and_preprocess_video(path4);
 
 %%
 for i=1:frameNum,
     % help I don't know how to display an RBG image (just displaying the R)
     display(i);
-    imagesc(video(:,:,1,i));
+    imagesc(video1(:,:,1,i));
     pause(0.05);
 end
 
 %% Filter the video and convert output to .AVI
+filtered1 = apply_filters(video1, gabors);
+% filtered2 = apply_filters(video2, gabors);
+% filtered3 = apply_filters(video3, gabors);
+% filtered4 = apply_filters(video4, gabors);
 
+%%
+for i=1:frameNum,
+    % displaying R channel and filter 1
+    imagesc(filtered1(:,:,1,i,1));
+    pause(0.05);
+end
 
-
+%%
+save('filtered1.mat', 'filtered1(:,:,:,:,1');
+% save('filtered2.mat', 'filtered2');
+% save('filtered3.mat', 'filtered2');
+% save('filtered4.mat', 'filtered3');
 
 
 
