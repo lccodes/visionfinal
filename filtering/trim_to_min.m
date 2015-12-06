99eadc81fe3c3f4d571ab75ad879674d532a6c0d
michellejzh <michelle_zheng@brown.edu>
2015-12-05 23:40:20 -0500

    cuts vids without enough frames

--- filtering/trim_to_min.m
+++ filtering/trim_to_min.m
@@ -1,23 +1,25 @@
-path = 'test_vids';
-% Return cell array of preprocessed videos from directory 'path'
-files = dir(path);
-files = files(4:end); % truncate '.' and '..' and 'DS thing'
-n_files = length(files);
-vids = cell(n_files,1);
-frameNums = zeros(n_files,1);
-display(sprintf('\nCurrently in folder "%s"',path));
+path = '../sorted';
 
-min = intmax;
-for i=1:n_files,
-    currFolder = sprintf('%s/%s', path, files(i).name);
-    currMin = min_frames(currFolder);
-    if currMin < min,
-        min = currMin;
-    end
-end
+% % Return cell array of preprocessed videos from directory 'path'
+% files = dir(path);
+% files = files(4:end); % truncate '.' and '..' and 'DS thing'
+% n_files = length(files);
+% vids = cell(n_files,1);
+% frameNums = zeros(n_files,1);
+% display(sprintf('\nCurrently in folder "%s"',path));
+% 
+% min = intmax;
+% for i=1:n_files,
+%     currFolder = sprintf('%s/%s', path, files(i).name);
+%     currMin = min_frames(currFolder);
+%     if currMin < min,
+%         min = currMin;
+%     end
+% end
+% 
+% display(sprintf('\nFinal min number of frames is "%d"',min));
 
-display(sprintf('\nFinal min number of frames is "%d"',min));
-
+min = 15;
 for i=1:n_files,
     currFolder = sprintf('%s/%s', path, files(i).name);
     trim_frames(currFolder,min);
