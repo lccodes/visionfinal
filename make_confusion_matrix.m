30bbdd1d46f2a080b0ee2dc8f71724b04ab8121b
Kei <kei@Keis-MacBook-Pro.local>
2015-12-07 22:03:44 -0500

    fix confusion matrix code

--- make_confusion_matrix.m
+++ make_confusion_matrix.m
@@ -1,8 +1,17 @@
 function mat=make_confusion_matrix(actual, predicted)
 len = size(actual, 1);
-mat = zeros(len);
+siize = 1;
+for ii = 1:len
+    if actual(ii) > siize
+        siize = actual(ii);
+    end
+    if predicted(ii) > siize
+        siize = predicted(ii);
+    end
+end
+mat = zeros(siize);
 
-assert(size(actual) == size(predicted));
+% assert(length(actual) == length(predicted));
 for ii = 1:length(actual)
    mat(predicted(ii), actual(ii)) =  mat(predicted(ii), actual(ii)) + 1;
 end
